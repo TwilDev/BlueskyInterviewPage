f3ebc898abd8e413c341e14202f4f3d34fa19e7a
Twilii <gola1_20@uni.worc.ac.uk>
2021-05-25 10:17:14 +0100

    Changing nav logic

--- js/nav.js
+++ js/nav.js
@@ -11,12 +11,18 @@ $(function(){
       openNavFlag = false;
       //trigger animation for hamburger and retract sidenav
       $('.hamburger--slider').removeClass("is-active");
-      $('.overlay-nav-sidebar').css("width", "0");
+      $('.overlay-nav-sidebar').css({"padding": "0", "width": "0"});
       return;
     }
     //if not open - open sidenav and trigger animation for hamburger
     $('.hamburger--slider').addClass("is-active");
-    $('.overlay-nav-sidebar').css("width", "100%");
+
+    if ($(window).width() > 768) {
+        $('.overlay-nav-sidebar').css({"padding": "1rem", "width": "40%"});
+    } else {
+      $('.overlay-nav-sidebar').css({"padding": "1rem", "width": "100%"});
+    }
+
     //set open state to true
     openNavFlag = true;
   });
